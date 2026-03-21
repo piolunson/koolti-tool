ef9cd250353054bae26327a13307e4732594d0c8
piolunson <piolunson@proton.me>
2026-03-21 19:19:16 +0100

    Update KooltiV9.py
    Some very im0ortant update

--- KooltiV9.py
+++ KooltiV9.py
@@ -2,13 +2,13 @@
 """
 KOOLTI-TOOL v8.2.1
 A terminal-based network & security toolkit.
-https://github.com/yourname/koolti-tool
+https://github.com/piolunson/koolti-tool
 
 Usage: python koolti_tool.py
 Requirements: pip install requests psutil rich
 """
 
-import os, sys, socket, requests, hashlib, random, string
+import os, sys, socket, requests, hashlib, random, string, multiprocessing
 import base64, re, itertools, time, platform, subprocess, tempfile
 from datetime import datetime
 from multiprocessing import Pool, cpu_count, Value
